60bdd9c89b5ed9ea6ac7a0aca9310ee3384b3890
nguyentai8893 <nguyenvantai566@gmail.com>
2024-06-12 08:04:21 +0700

    lại

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -105,7 +105,7 @@ const Navbar = () => {
 					</NavLink>
 				</div>
 				<div className={classloginUser}>
-					{user.length ? (
+					{user ? (
 						<>
 							<div className={classes.flexIcon}>
 								<FontAwesomeIcon icon={faUser} className={classes.icon} />
